9277268bb76ec1bcc36ce58279b8c7a75ef70ad0
Rob Culp <rlculp3@gmail.com>
2018-06-12 15:43:45 -0400

    added css linting, and updated the rewired config override

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,11 @@
 const rewireEslint = require('react-app-rewire-eslint');
+const rewireStylelint = require('react-app-rewire-stylelint');
+
 module.exports = function override(config, env) {
- config = rewireEslint(config, env);
- return config;
+  config = rewireEslint(config, env);
+  config = rewireStylelint.withLoaderOptions({
+    files: ['src/**/*.css']
+  })(config, env);
+
+  return config;
 }
