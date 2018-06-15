c2f346b470f4d6c45a71d1583c31e7488d6c2de1
Kevin King <kking.personal@gmail.com>
2018-06-15 13:44:19 -0400

    Got index file testing

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
+
+import { store, history } from 'redux/store';
+
+import Home from 'containers/Home/Home';
+import Results from 'containers/Results/Results';
+
+const App = () => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/results" component={Results} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -5,26 +5,9 @@ import registerServiceWorker from 'registerServiceWorker';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import { ConnectedRouter } from 'connected-react-router';
 
-import { store, history } from 'redux/store';
+import App from 'components/App/App';
 
-import Home from 'containers/Home/Home';
-import Results from 'containers/Results/Results';
-
-ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/results" component={Results} />
-        <Redirect from="*" to="/" />
-      </Switch>
-    </ConnectedRouter>
-  </Provider>
-  , document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
