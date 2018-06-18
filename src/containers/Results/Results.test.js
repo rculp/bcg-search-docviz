f8d61323789f757390737201b5fee1dc210fffba
Kevin King <kking.personal@gmail.com>
2018-06-18 18:56:25 -0400

    Got handleChange tested (whew)

--- src/containers/Results/Results.test.js
+++ src/containers/Results/Results.test.js
@@ -11,7 +11,7 @@ import ConnectedResultsContainer, { ResultsContainer } from './Results';
 
 describe('Results', () => {
   const mockStore = configureStore();
-  const initialState_full = {
+  const initialState = {
     search: {
       results: {
         Result: {
@@ -28,45 +28,33 @@ describe('Results', () => {
       }
     }
   };
-  const initialState_noResult = {
-    search: {
-      results: {
-        Result: null
-      }
-    }
-  };
-  const initialState_noDocsReturned = {
-    search: {
-      results: {
-        Result: {
-          Docs: []
-        }
-      }
-    }
-  };
+
   const mockHistory = {push: jest.fn()};
   let store;
 
   beforeEach(() => {
     jest.clearAllMocks();
-    store = mockStore(initialState_full);
+    store = mockStore(initialState);
   });
 
   it('connects to redux store', () => {
     const component = shallow(
       <ConnectedResultsContainer store={store} />
     );
-    expect(component.props().results).toEqual(initialState_full.search.results);
+    expect(component.props().results).toEqual(initialState.search.results);
   });
 
   it('renders and matches our snapshot', () => {
     const component = shallow(
-      <ResultsContainer results={initialState_full.search.results} history={mockHistory}/>
+      <ResultsContainer results={initialState.search.results} history={mockHistory}/>
     );
     expect(component).toMatchSnapshot();
   });
 
   it('shows message if no docs are found', () => {
+    const initialState_noDocsReturned = {...initialState};
+    initialState_noDocsReturned.search.results.Result.Docs = [];
+
     const component = shallow(
       <ResultsContainer results={initialState_noDocsReturned.search.results} history={mockHistory}/>
     );
@@ -74,6 +62,9 @@ describe('Results', () => {
   });
 
   it('redirects to / if no results are found', () => {
+    const initialState_noResult = {...initialState};
+    initialState_noResult.search.results.Result = null;
+
     const component = shallow(
       <ResultsContainer results={initialState_noResult.search.results} history={mockHistory}/>
     );
