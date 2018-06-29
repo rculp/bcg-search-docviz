e819c8dc92a3c6f465ad088e2e05203b4fd0e0fe
Kevin King <kking.personal@gmail.com>
2018-06-29 13:47:19 -0400

    Bound every field in UI to something from mock server

--- src/containers/Results/Results.js
+++ src/containers/Results/Results.js
@@ -1,16 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import uuid from 'uuid/v1';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actions as searchActions, selectors as searchSelectors } from 'redux/search/search';
-import uuid from 'uuid/v1';
 import ReactHtmlParser from 'react-html-parser';
-import { Grid, Row, Col } from 'react-flexbox-grid';
 
 import Heading from 'components/Heading/Heading';
-import RefinerTagCloud from 'components/RefinerTagCloud/RefinerTagCloud';
-import RefinerTree from 'components/RefinerTree/RefinerTree';
-import RefinerList from 'components/RefinerList/RefinerList';
 import Card from 'components/Card/Card';
 import Message from 'components/Message/Message';
 
@@ -19,66 +15,40 @@ import Page from 'components/Page/Page';
 import './Results.scss';
 
 export const ResultsContainer = ({ history, results }) => {
-  if (!results.Result) {
+  if (!results.results) {
     history.push('/');
     return null;
   }
   return (
     <Page id="results">
       {
-        results.Result.Docs.length <= 0 &&
+        results.results.length <= 0 &&
         <Message header="No Results Found" content="Please try a different search." />
       }
       {
-        results.Result.Docs.length > 0 &&
-        <div>
-          <Heading>Showing 1 - 20 of 432 results found</Heading>
-          <Card fluid color="green">
-            <Card.Content>
-              <Card.PracticeArea>Industrial Goods &bull; Technology Advantage</Card.PracticeArea>
-              <Card.MatchPercentage>83% Match</Card.MatchPercentage>
-              <Card.Header>Digital trends as an enabler for digital transformation: How can TA support you on Digital transformation?</Card.Header>
-              <Card.Meta>UPDATED: 12 OCT 2018</Card.Meta>
-              <Card.Description>With the advent of AI and Intelligent Automation capabilies, the possibilites for digital transformation to create a shift in healthcare providers mindsets is proving a challenge to conventional regulation…</Card.Description>
-            </Card.Content>
-          </Card>
-          <Grid>
-            <Row>
-              <Col xs={4}>
-                <Heading as="h1">Refiners</Heading>
-                {
-                  results.Result.Boxes.map((box) => {
-                    if (box.type === 'TagCloud') {
-                      return <RefinerTagCloud key={uuid()} box={box} />;
-                    } else if (box.type === 'Tree') {
-                      return <RefinerTree key={uuid()} box={box} />;
-                    } else if (box.type === 'List') {
-                      return <RefinerList key={uuid()} box={box} />;
+        results.results.length > 0 &&
+        <Fragment>
+          <Heading>Showing 1 - { results.results.length } of { results.totalHitCount } results found</Heading>
+          {
+            results.results.map(doc => (
+              <Card key={uuid()} fluid color="green">
+                <Card.Content>
+                  <Card.PracticeArea>
+                    {
+                      doc.industryPA.map((pa, index) => (
+                        <span key={uuid()}>{ index > 0 && <span>&nbsp;&bull;&nbsp;</span> }{pa}</span>
+                      ))
                     }
-                    return <div key={uuid()}>Unrecognized refiner type</div>;
-                  })
-                }
-              </Col>
-              <Col xs={8}>
-                <Heading as="h1">Results</Heading>
-                {
-                  results.Result.Docs.map(doc => (
-                    <Grid key={uuid()}>
-                      <Row>
-                        <Col xs={6}>
-                          {doc.filename}
-                        </Col>
-                        <Col xs={6}>
-                          { ReactHtmlParser(doc.largesummaryhtml) }
-                        </Col>
-                      </Row>
-                    </Grid>
-                  ))
-                }
-              </Col>
-            </Row>
-          </Grid>
-        </div>
+                  </Card.PracticeArea>
+                  <Card.MatchPercentage>{ doc.relevancyScore * 100 }% Match</Card.MatchPercentage>
+                  <Card.Header>{ doc.title }</Card.Header>
+                  <Card.Meta>UPDATED: { new Date(doc.uploadDate).toLocaleDateString() }</Card.Meta>
+                  <Card.Description>{ ReactHtmlParser(doc.smallSummaryHtml) }</Card.Description>
+                </Card.Content>
+              </Card>
+            ))
+          }
+        </Fragment>
       }
     </Page>
   );
